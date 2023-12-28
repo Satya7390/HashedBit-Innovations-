f32afe3d8cd4834bb1407fdf453efc089a0e1b64
satya <satyanandgupta845@gmail.com>
2023-12-28 19:58:05 +0530

    Assignment-8 done

--- React-folder/src/Home2.js
+++ React-folder/src/Home2.js
@@ -7,6 +7,7 @@ const Home2 = () => {
             <div className="row">
                 <div className="col-6"><h2>Hii Satya !!</h2></div>
                 <div className="col-6"><h2>What are you doing !!</h2></div>
+                <div></div>
             </div>
         </div>
         </>
